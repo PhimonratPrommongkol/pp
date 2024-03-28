7bccfec217d5feccb3481d36aec0533216313ec8
ChanonOonna <Chanon.oo@ku.th>
2024-03-29 04:41:53 +0700

    up code sql

--- sql/teachers.sql
+++ sql/teachers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Mar 28, 2024 at 07:42 PM
+-- Generation Time: Mar 28, 2024 at 09:41 PM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.8
 
@@ -31,17 +31,18 @@ CREATE TABLE `teachers` (
   `id` int NOT NULL,
   `firstname_teacher` varchar(255) NOT NULL,
   `lastname_teacher` varchar(255) NOT NULL,
-  `education_level` enum('ไทย','สังคม','คณิต','ประวิติศาสตร์','ภูมิศาสตร์','อังกฤษ','พละ','ฟิสิกส์','เคมี','ชีวะ','จีน') NOT NULL,
-  `timein` int NOT NULL,
-  `timeout` int NOT NULL
+  `subject_select` enum('ไทย','สังคม','คณิต','ประวิติศาสตร์','ภูมิศาสตร์','อังกฤษ','พละ','ฟิสิกส์','เคมี','ชีวะ','จีน') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `timein` varchar(255) NOT NULL,
+  `timeout` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `teachers`
 --
 
-INSERT INTO `teachers` (`id`, `firstname_teacher`, `lastname_teacher`, `education_level`, `timein`, `timeout`) VALUES
-(1, 'ฟหกก', 'ฟหก', 'เคมี', 1234, 2367);
+INSERT INTO `teachers` (`id`, `firstname_teacher`, `lastname_teacher`, `subject_select`, `timein`, `timeout`) VALUES
+(1, 'ฟหกก', 'ฟหก', 'เคมี', '1234', '2367'),
+(2, 'asd', 'asd', 'ประวิติศาสตร์', '12:11', '12:12');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +62,7 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
